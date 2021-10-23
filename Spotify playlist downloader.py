8ef77f5272b8a1c3363ac9c2131b91953dfa578e
rickyrorton <74890659+rickyrorton@users.noreply.github.com>
2021-10-23 15:18:35 +0400

    removed requirement for ffmpeg
    as the need for ffmpeg has been removed songs are converted much faster but only to the m4a audio file format

--- Spotify playlist downloader.py
+++ Spotify playlist downloader.py
@@ -1,11 +1,9 @@
 import sys
 from os import path as ospath
-from os import remove
-from tkinter import CENTER, Button, Entry, Label, StringVar, Tk,scrolledtext,INSERT,OptionMenu,LEFT
+from os import remove,rename
+from tkinter import CENTER, Button, Entry, Label, Tk,scrolledtext,INSERT,LEFT
 from urllib import request
-from eyed3 import load as mdload
-from eyed3.id3 import ID3_V2_3
-from pydub import AudioSegment
+from mutagen import easymp4,mp4
 from pytube import YouTube
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -17,23 +15,27 @@ from tkinter.filedialog import askdirectory
 
 client_credentials_manager = SpotifyClientCredentials(client_id='', client_secret='')
 sp = Spotify(client_credentials_manager=client_credentials_manager)
+
 stop=False
+
 if getattr(sys, 'frozen', False):
         application_path = ospath.dirname(sys.executable)
 elif __file__:
-    application_path = ospath.dirname(__file__)
+    application_path = ospath.dirname(__file__)  
+
 def task(tracks):
     global application_path
     global window
     global stop
+
     download_but.config(state='disabled',text='Downloading')
-    quality=variable.get()[-4::1]
-    quality=quality.replace('-','')
+
     global location
     try:
         download_path=location
     except:
         download_path=ospath.join(application_path,'Downloads')
+
     for i in tracks:
         window.update()
         j=i['track']
@@ -42,43 +44,51 @@ def task(tracks):
             results = YoutubeSearch(song, max_results=1).to_dict()
             vid_url='http://youtu.be'+results[0]['url_suffix'].replace('watch?v=','')
             yt=YouTube(vid_url.replace('watch?v=','')).streams.get_audio_only()
-            yt.download(download_path)
+            m4a_name=''
+            for i in j['artists'][0]['name']+'-'+j['name']:
+                if i in ['/','\\','?','%','*',':','|','"','<','>','.',',',';','=']:
+                    m4a_name+=' '
+                else:
+                    m4a_name+=i
+            download_name=m4a_name+'.mp4'
+            m4a_name+='.m4a'
+            mp4path=ospath.join(download_path,download_name)
+            m4apath=ospath.join(download_path,m4a_name)
 
-            scrolled.insert(INSERT,'Thread sucessfully downloaded {}\n'.format(j['name']))
+            if not ospath.exists(m4apath) or ospath.exists(mp4path):
+                yt.download(download_path,download_name)
+                scrolled.insert(INSERT,'Thread sucessfully downloaded {}\n'.format(j['name']))
     
         except Exception as e:
             yt=YouTube(vid_url).streams.get_audio_only()
             print(e,'Couldn\'t download',song,'result:',yt)
 
-        try:
-            #converting song
-            webm = AudioSegment.from_file(ospath.join(download_path,yt.default_filename))
-            duration_s=j['duration_ms']/1000
-            if webm.duration_seconds > duration_s:
-                export_song=webm[:duration_s*1000]
-            else:
-                export_song=webm
-            export_song.export(ospath.join(download_path,str(j['artists'][0]['name']+'-'+j['name']+'.mp3').replace('"','')),format='mp3',bitrate=quality)
-            #adding metadata
-            audiofile=mdload(ospath.join(download_path,str(j['artists'][0]['name']+'-'+j['name']+'.mp3').replace('"','')))
-            if not audiofile.tag:
-                audiofile.initTag()
-            request.urlretrieve(j['album']['images'][0]['url'],ospath.join(download_path,str(j['artists'][0]['name']+'-'+j['name']+'.jpg').replace('"','')))
-            tag=audiofile.tag
-            tag.artist = j['artists'][0]['name']
-            tag.album = j['album']['name']
-            tag.title = j['name']
-            tag.album_artist = j['album']['artists'][0]['name']
-            tag.recording_date=j['album']['release_date'][0:4]
-            tag.images.set(3, open(ospath.join(download_path,str(j['artists'][0]['name']+'-'+j['name']+'.jpg').replace('"','')),'rb').read(), 'image/jpeg')
-            tag.save(version=ID3_V2_3)
-            #deleting stuff
-            remove(ospath.join(download_path,yt.default_filename))
-            remove(ospath.join(download_path,str(j['artists'][0]['name']+'-'+j['name']+'.jpg').replace('"','')))
-            
-            scrolled.insert(INSERT,'Converted {}\n'.format(j['name']))
-        except Exception as e:
-            print('Couldnt convert song',e)
+        global convertcheck
+        if convertcheck and ospath.exists(mp4path):
+            try:
+                #converting song
+                rename(mp4path,m4apath)
+                #adding metadat
+                tags=easymp4.EasyMP4(m4apath)
+                if not tags.tags:
+                    tags.add_tags()
+                tags['artist'] = j['artists'][0]['name']
+                tags['album'] = j['album']['name']
+                tags['title'] = j['name']
+                tags['albumartist'] = j['album']['artists'][0]['name']
+                tags['date']=j['album']['release_date'][0:4]
+                tags.save()
+                #adding cover art
+                coverart=mp4.MP4(m4apath)
+                iconname=ospath.join(download_path,str(j['artists'][0]['name']+'-'+j['name']+'.jpg').replace('"',''))
+                request.urlretrieve(j['album']['images'][0]['url'],iconname)
+                with open(iconname,'rb') as f:
+                    coverart['covr'] = [mp4.MP4Cover(f.read(),imageformat=mp4.MP4Cover.FORMAT_JPEG)]
+                coverart.save()
+                remove(iconname)            
+                scrolled.insert(INSERT,'Converted {}\n'.format(j['name']))
+            except Exception as e:
+                print('Couldnt convert song',e)
         if stop==True:
             break  
     download_but.config(state='normal',text='Download songs')
@@ -117,6 +127,7 @@ if __name__=='__main__':
     window.configure(bg = '#3d3d3d')
     window.title('Spotify playlist downloader')
     window.protocol('WM_DELETE_WINDOW',stoptrue)
+
     def image_import(filename,height,width):
         try:
             base_path = sys._MEIPASS
@@ -131,6 +142,7 @@ if __name__=='__main__':
         img=img.resize((height,width), Image.ANTIALIAS)
         pic=ImageTk.PhotoImage(img)
         return pic
+
     logo=image_import('logo.png',48,48)
     lbl=Label(window,text=' SPOTIFY PLAYLIST DOWNLOADER',font = ("Arial Bold",14),bg = '#3d3d3d', fg = 'white',image=logo,compound=LEFT)
     lbl.place(relx=0.5,rely=0.08,anchor=CENTER)
@@ -144,15 +156,6 @@ if __name__=='__main__':
     url=Entry(window,width=60)
     url.place(relx=0.59,rely=0.17,anchor=CENTER)
     url.bind('<Return>', start_downloader)
-    
-    audio_quality=['Low-96k','Medium-128k','High-192k','Ultra high-320k']
-    variable=StringVar(window)
-    variable.set(audio_quality[1])
-    dropdown=OptionMenu(window,variable,*audio_quality)
-    dropdown.place(relx=0.35,rely=0.78,anchor=CENTER)
-
-    lbl3=Label(window,text='Audio Quality:',font = ("Arial Bold",9),bg = '#3d3d3d', fg = 'white')
-    lbl3.place(relx=0.14,rely=0.78,anchor=CENTER)
 
     lbl4=Label(window,text='Download location: '+str(ospath.join(application_path,'Downloads')),font = ("Arial Bold",9),bg = '#3d3d3d', fg = 'white')
     lbl4.place(relx=0.5,rely=0.85,anchor=CENTER)
@@ -162,12 +165,32 @@ if __name__=='__main__':
     scrolled.place(relx=0.5,rely=0.48,anchor=CENTER)
 
     download_but=Button(window,text='Download songs',bg='grey',fg='white',font = ("Arial",12),command=start_downloader)
-    download_but.place(relx=0.5,rely=0.93,anchor=CENTER)
+    download_but.place(relx=0.7,rely=0.93,anchor=CENTER)
     
+    convertlbl=Label(window,text='Convert songs:',font = ("Arial Bold",9),bg = '#3d3d3d', fg = 'white')
+    convertlbl.place(relx=0.15,rely=0.93,anchor=CENTER)
+    global convertcheck
+    convertcheck=True
+
+    def convert():
+        global convertcheck
+        if convertcheck:
+            convertbut.config(text='OFF')
+            convertcheck=False
+        else:
+            convertbut.config(text='ON')
+            convertcheck=True
+       
+    convertbut=Button(window,text='ON',bg='grey',fg='white',font = ("Arial",12),command=convert)
+    convertbut.place(relx=0.29,rely=0.93,anchor=CENTER)
+
     def directrory():
         global location
         location=askdirectory()
-        lbl4.config(text='Download location:'+str(location))
+        if location:
+            lbl4.config(text='Download location:'+str(location))
+        else:
+            pass
 
     file=Button(window,text='Change download folder',bg='grey',fg='white',font = ("Arial",12),command=directrory)
     file.place(relx=0.7,rely=0.78,anchor=CENTER)
