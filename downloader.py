961b28cb022b8d654349dcc15f650c4bc4af30f4
rickyrorton <74890659+rickyrorton@users.noreply.github.com>
2022-01-20 12:49:32 +0400

    New source code

---
+++ downloader.py
@@ -0,0 +1,191 @@
+from os import path as ospath
+from os import remove,rename
+from urllib import request
+from mutagen.mp4 import MP4,MP4Cover
+from mutagen.id3 import ID3,TIT2,APIC,TALB,TPE1,TPE2,TYER
+from pytube import YouTube
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyClientCredentials
+from youtube_search import YoutubeSearch
+from threading import Thread
+from datetime import datetime
+from time import time as ttime
+from pydub import AudioSegment
+from tkinter import messagebox
+
+client_credentials_manager = SpotifyClientCredentials(client_id='', client_secret='')
+sp = Spotify(client_credentials_manager=client_credentials_manager)
+
+def remove_sus_characters(name:str):
+    converted = "".join(i for i in name if i not in ("/", "\\", "?", "%", "*", ":", "|", '"', "<", ">", ".", ",", ";", "="))
+    return converted
+
+def accusearch(results,songlen):
+    for i in results:
+        time = datetime.strptime(i['duration'], '%M:%S')
+        vid_length = time.minute*60+time.second
+        if vid_length >= songlen+5 or vid_length <= songlen-5:
+            pass
+        else:
+            vid_url = 'http://youtu.be'+ i['url_suffix'].replace('watch?v=', '')
+            break
+    return YouTube(vid_url)
+
+def m4atagger(file,song,path):
+    iconname=ospath.join(path,remove_sus_characters(song['artists'][0]['name']+'-'+song['name'])+'.jpg')
+    request.urlretrieve(song['album']['images'][0]['url'],iconname)
+    tags=MP4(file)
+    if not tags.tags:
+        tags.add_tags()
+    tags[u'\xa9nam']=song['name']
+    tags[u'\xa9alb']=song['album']['name']
+    tags[u'\xa9ART']=', '.join([i['name'] for i in song['artists']])
+    tags[u'aART']=', '.join([i['name'] for i in song['album']['artists']])
+    tags[u'\xa9day']=song['album']['release_date'][0:4]
+    with open(iconname,'rb') as f:
+        tags['covr'] = [MP4Cover(f.read(),imageformat=MP4Cover.FORMAT_JPEG)]
+    tags.save()
+    remove(iconname)
+
+def mp3convtagger(mp4,mp3,song,path):
+    iconname=ospath.join(path,remove_sus_characters(song['artists'][0]['name']+'-'+song['name'])+'.jpg')
+    request.urlretrieve(song['album']['images'][0]['url'],iconname)
+    convert=AudioSegment.from_file(mp4)
+    convert.export(mp3,format='mp3')
+    tags=ID3(mp3)
+    tags.add(TIT2(encoding=3, text=[song['name']]))
+    tags.add(TALB(encoding=3, text=[song['album']['name']]))
+    tags.add(TPE1(encoding=3, text=[i['name'] for i in song['artists']]))
+    tags.add(TPE2(encoding=3, text=[i['name'] for i in song['album']['artists']]))
+    tags.add(TYER(encoding=3, text=[song['album']['release_date'][0:4]]))
+    with open(iconname, "rb") as f:
+        tags.add(APIC(encoding=3,mime=u'image/jpeg',type=3, desc=u'Cover',data=f.read()))
+    tags.save(v2_version=3)
+    remove(mp4)
+    remove(iconname)
+
+def start(dlbut,scrltxt,progress,link:str,path:str,threadno:int,filetype:str):
+    global threads
+    global leader
+    try:
+        for t in threads: 
+            t.join()
+        for t in leader:
+            t.join()
+    except:
+        pass
+    threads=[]
+    leader=[]
+    if link.startswith('https://open.spotify.com/track'):
+        dlbut['state']='disabled'
+        progress['maximum']=1
+        for i in range(1):
+            t=Thread(target=download_song,args=(link,scrltxt,path,filetype,dlbut,progress),daemon=False)
+            t.start()
+            threads.append(t)
+    elif link.startswith('https://open.spotify.com/playlist/'):
+        dlbut['state']='disabled'
+        playlist=sp.playlist_tracks(link)
+        name=sp.playlist(link)["name"]
+        tracks=playlist['items']
+        while playlist['next']:
+            playlist=sp.next(playlist)
+            tracks.extend(playlist)
+        progress['maximum']=len(tracks)
+        scrltxt.insert('insert','Downloading playlist \"{}\" with {} songs\n'.format(name,len(tracks)))
+        lead=True
+        for i in range(threadno):
+            t=Thread(target=download_playlist,args=(tracks[i::threadno],scrltxt,path,filetype,lead,dlbut,i+1,progress),daemon=False)
+            t.start()
+            if not lead:
+                threads.append(t)
+            else:
+                leader.append(t)          
+            lead=False
+    else:
+        messagebox.showerror('Invalid link','You have given an invalid link,try again this time but with a correct link')
+
+def download_song(link,scrltxt,path,filetype,button,progress):
+    song=sp.track(link)
+    results = YoutubeSearch(song['name']+' '+song['artists'][0]['name']+' audio', max_results=15).to_dict()
+    spsonglen = int((song['duration_ms'])/1000)
+    download_name=remove_sus_characters(song['artists'][0]['name']+'-'+song['name'])
+    vid=accusearch(results=results,songlen=spsonglen)
+    mp4path=ospath.join(path,download_name+'.mp4')
+    if filetype=='.m4a':
+        if not ospath.exists(ospath.join(path,download_name+'.m4a')):
+            try:
+                yt=vid.streams.get_audio_only()
+                yt.download(path,download_name+'.mp4')
+                m4apath=ospath.join(path,download_name+'.m4a')
+                rename(mp4path,m4apath)
+                m4atagger(m4apath,song,path)
+            except Exception as e:
+                messagebox.showerror('Error','Oops program couldnt download {} because of {}'.format(song['name'],e))
+        
+    if filetype=='.mp3':
+        if not ospath.exists(ospath.join(path,download_name+'.mp3')):
+            try:
+                yt=vid.streams.get_audio_only()
+                yt.download(path,download_name+'.mp4')
+                mp3path=ospath.join(path,download_name+'.mp3')
+                mp3convtagger(mp4path,mp3path,song,path)
+            except Exception as e:
+                messagebox.showerror('Error','Oops program couldnt download {} because of {}'.format(song['name'],e))
+    '''if filetype=='.webm':
+        if not ospath.exists(ospath.join(path,download_name+'.webm')):
+            yt=vid.streams.filter(mime_type='audio/webm').order_by('abr').desc()[0]
+            yt.dowload(path,download_name+'.webm')
+            webmpath=ospath.join(path,download_name+'.webm')
+            #webmtagger()'''
+    scrltxt.insert('insert','Finished downloading and converting {}\n'.format(song['name']))
+    scrltxt.see('end')
+    progress['value']=1
+    button['state']='normal'
+    messagebox.showinfo("Song has finished downloading","The song has finished downloading")
+    progress['value']=0
+
+def download_playlist(tracks,scrltxt,path,filetype,leader,button,number,progress):
+    for i in tracks:
+        song=i['track']
+        results = YoutubeSearch(song['name']+' '+song['artists'][0]['name']+' audio', max_results=15).to_dict()
+        spsonglen = int((song['duration_ms'])/1000)
+        download_name=remove_sus_characters(song['artists'][0]['name']+'-'+song['name'])
+        vid=accusearch(results=results,songlen=spsonglen)
+        mp4path=ospath.join(path,download_name+'.mp4')
+        if filetype=='.m4a':
+            if not ospath.exists(ospath.join(path,download_name+'.m4a')):
+                try:
+                    yt=vid.streams.get_audio_only()
+                    yt.download(path,download_name+'.mp4')
+                    m4apath=ospath.join(path,download_name+'.m4a')
+                    rename(mp4path,m4apath)
+                    m4atagger(m4apath,song,path)
+                except Exception as e:
+                    messagebox.showerror('Error','Oops program couldnt download {} because of {}'.format(song['name'],e))
+            
+        if filetype=='.mp3':
+            if not ospath.exists(ospath.join(path,download_name+'.mp3')):
+                try:
+                    yt=vid.streams.get_audio_only()
+                    yt.download(path,download_name+'.mp4')
+                    mp3path=ospath.join(path,download_name+'.mp3')
+                    mp3convtagger(mp4path,mp3path,song,path)
+                except Exception as e:
+                    messagebox.showerror('Error','Oops program couldnt download {} because of {}'.format(song['name'],e))
+        
+        '''if filetype=='.webm':
+            if not ospath.exists(ospath.join(path,download_name+'.webm')):
+                yt=vid.streams.filter(mime_type='audio/webm').order_by('abr').desc()[0]
+                yt.dowload(path,download_name+'.webm')
+                webmpath=ospath.join(path,download_name+'.webm')
+                #webmtagger()'''
+        scrltxt.insert('insert','Thread {} Finished downloading and converting {}\n'.format(number,song['name']))
+        scrltxt.see('end')
+        progress['value']+=1
+    if leader:
+        global threads
+        for i in threads:
+            i.join()
+        button['state']='normal'
+        messagebox.showinfo("Songs have finished downloading","All the songs have finished downloading")
---
+++ gui.py
@@ -0,0 +1,130 @@
+#importing necessary libraries
+from tkinter import Entry, StringVar, Tk,Button,Label,scrolledtext,LabelFrame,CENTER,OptionMenu,Scale, HORIZONTAL,LEFT#tkinter for the user interface
+from PIL import Image,ImageTk #Python(PIL) image library for inserting images into the user interface
+from tkinter.filedialog import askdirectory
+from tkinter.ttk import Progressbar
+import sys
+from os import path as ospath
+from threading import Thread
+import downloader
+
+#defining a window
+global window
+window=Tk()
+window.geometry('600x700')
+window.resizable(False,False)
+window.configure(bg = '#3d3d3d')
+window.title('Spotify Downloader')
+
+#Widgets in the window
+title=Label(window,text=' SPOTIFY DOWNLOADER',font = ("Arial Bold",18),bg = '#3d3d3d', fg = 'white')
+title.place(relx=0.5,rely=0.05 ,anchor=CENTER)
+
+entry_label=Label(window,text='Enter song/playlist link:',font = ("Arial Bold",10),bg = '#3d3d3d', fg = 'white')
+entry_label.place(relx=0.15,rely=0.12,anchor=CENTER)
+
+output_label=Label(window,text='OUTPUT:',font = ("Arial Bold",12),bg = '#3d3d3d', fg = 'white')
+output_label.place(relx=0.5,rely=0.17,anchor=CENTER)
+
+download_location=Label(window,text='Download location:',font = ("Arial Bold",10),bg = '#3d3d3d', fg = 'white',justify=LEFT)
+download_location.place(relx=0.5,rely=0.87,anchor=CENTER)
+
+thread_number=Label(window,text='How many threads to use?\n(More threads=more cpu usage)',font = ("Arial Bold",10),bg = '#3d3d3d', fg = 'white')
+thread_number.place(relx=0.2,rely=0.8,anchor=CENTER)
+
+filetype=Label(window,text='Filetype:',font = ("Arial Bold",10),bg = '#3d3d3d', fg = 'white')
+filetype.place(relx=0.73,rely=0.8,anchor=CENTER)
+
+global progress
+progress=Progressbar(window,orient = HORIZONTAL,mode = 'determinate',length=100)
+progress.place(relx=0.5,rely=0.72,width=400,anchor=CENTER)
+
+scrolled_cont = LabelFrame(window, font=("Arial Bold", 15), background='#1DB954', foreground='white', borderwidth=5, labelanchor="n")
+scrolled_cont.place(relx=0.5,rely=0.43,height=330,width=510,anchor=CENTER)
+
+global output_box
+output_box=scrolledtext.ScrolledText(window, font = ("Arial",10),bg='#3d3d3d',fg='white')
+output_box.place(relx=0.5,rely=0.43,height=320,width=500,anchor=CENTER)
+
+dl_location_button=Button(window,text='Change download folder',fg='#3d3d3d',bg='white',font = ("Arial",14),command=lambda:directrory())
+dl_location_button.place(relx=0.3,rely=0.94,anchor=CENTER)
+
+global download_button
+download_button=Button(window,text='Download songs',fg='#3d3d3d',bg='white',font = ("Arial",14),command=lambda:start_downloader())#,image=dl_logo,compound=LEFT)
+download_button.place(relx=0.7,rely=0.94,anchor=CENTER)
+
+global filetype_default
+filetypes=['.m4a','.mp3']
+filetype_default=StringVar()
+filetype_default.set('.m4a')    
+filetype_dropdown=OptionMenu(window,filetype_default,*filetypes)
+filetype_dropdown.place(relx=0.8,rely=0.78)
+
+global thread_num_set
+thread_num_set=Scale(window,from_=1,to=20,tickinterval=19,orient=HORIZONTAL,highlightbackground='#3d3d3d',background='#3d3d3d',fg='white')
+thread_num_set.set(4)
+thread_num_set.place(relx=0.4,rely=0.76)
+
+entry_cont = LabelFrame(window, font=("Arial Bold", 15), background='#1DB954', foreground='white', borderwidth=5, labelanchor="n")
+entry_cont.place(relx=0.62,rely=0.12,width=394,height=24,anchor=CENTER)
+
+global playlist_link
+playlist_link=Entry(window,bg='#3d3d3d',fg='white')
+playlist_link.place(relx=0.62,rely=0.12,width=390,height=20,anchor=CENTER)
+
+#getting path to the directory
+if getattr(sys, 'frozen', False):
+        application_path = ospath.dirname(sys.executable)
+elif __file__:
+    application_path = ospath.dirname(__file__) 
+
+#setting default download location
+global location
+location=ospath.join(application_path,'Downloads').replace('\\','/')
+download_location.config(text='Download location:'+str(location))
+
+#function for importing pictures into the gui
+def image_import(filename,height,width):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = ospath.abspath(".")
+    try:
+        image_path=ospath.join(base_path,filename)
+        img=Image.open(image_path)
+    except:
+        image_path=ospath.join(application_path,filename)
+        img=Image.open(image_path)
+    img=img.resize((height,width), Image.ANTIALIAS)
+    pic=ImageTk.PhotoImage(img)
+    return pic
+
+#The actual images being imported
+logo=image_import('logo.png',48,48)
+title.config(image=logo,compound=LEFT)
+
+download_logo=image_import('dl_logo.png',40,40)
+download_button.config(image=download_logo,compound=LEFT)
+
+#download location related stuff
+def directrory():
+        global location
+        location=askdirectory()
+        if location:
+            download_location.config(text='Download location:'+str(location))
+        else:
+            pass
+
+def start_downloader():
+    global location
+    global output_box
+    global download_button
+    global window
+    global progress
+    link=playlist_link.get()
+    playlist_link.delete(0,len(link))
+    threads=thread_num_set.get()
+    filetype=filetype_default.get() 
+    downloader.start(dlbut=download_button,link=link,path=location,threadno=threads,filetype=filetype,scrltxt=output_box,progress=progress)  
+      
+window.mainloop() 
