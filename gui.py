ef7f7903ae692e91ff5439b40563143e3dedd28e
the-batfish <74861519+the-batfish@users.noreply.github.com>
2023-01-11 21:00:24 +0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -35,7 +35,7 @@ class App(Tk):
         self.geometry("600x600")
         self.resizable(False, False)
         self.configure(bg="#333333")
-        self.title("Spotitube Downloader")
+        self.title("Spotify Downloader")
         self.iconphoto(True, self.image_import("./img/logo.ico", 32, 32))
 
 
@@ -68,7 +68,7 @@ class App(Tk):
         self.logo = self.image_import("img/logo.png", 48, 48)
         title = ttk.Label(
             self,
-            text=" SPOTITUBE DOWNLOADER",
+            text=" SPOTIFY DOWNLOADER",
             image=self.logo,
             compound=LEFT,
             font=("Helvetica", 20),
